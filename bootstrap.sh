eb7b9db10fc6206e8cc5240e179dae32695f3abb
oliveagle <oliveagle@gmail.com>
2015-10-27 17:18:22 +0800

    manage dependencies locally

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,12 @@ if [ ! -d .venv ]; then
   virtualenv --no-site-packages .venv
 fi
 
-source .venv/bin/activate && \
-  pip install distribute pbr && \
-  pip install -r requirements.txt
+#source .venv/bin/activate && \
+#  pip install distribute pbr && \
+#  pip install -r requirements.txt
+
+# already download packages from internet. 
+OPTS="--no-index --find-links=../python_packages -U"
+soruce .venv/bin/activate && \
+  pip install $OPTS setuptools distribute pbr && \
+  pip install $OPTS -r requirements.txt
